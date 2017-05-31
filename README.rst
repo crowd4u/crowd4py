04a8da339c89c92a90de5a9610ec6d52ababfef9
sshayashi <sshayashi0208@gmail.com>
2017-05-31 17:52:10 +0900

    fix name

--- README.rst
+++ README.rst
@@ -1,6 +1,4 @@
-Sample Module Repository
-========================
+Crowd4py: Crowd4u for Python!
+============================
 
-This simple project is an example repo for Python projects.
-
-`Learn more <http://www.kennethreitz.org/essays/repository-structure-and-python>`_.
+This  project is an example repo for Python projects.
--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
+from .context import crowd4py
 
 import unittest
 
@@ -9,7 +9,7 @@ class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_thoughts(self):
-        self.assertIsNone(sample.hmm())
+        self.assertIsNone(crowd4py.hmm())
 
 
 if __name__ == '__main__':
