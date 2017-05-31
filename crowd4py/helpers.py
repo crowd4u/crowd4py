3bbd5f81b75fe01c32163df22cc8bc7f7ff8a33d
sshayashi <sshayashi0208@gmail.com>
2017-05-31 18:34:40 +0900

    helpre関数の追加

--- crowd4py/helpers.py
+++ crowd4py/helpers.py
@@ -1,3 +1,22 @@
-def get_answer():
-    """Get an answer."""
-    return True
+def get_post_url(etroot):
+    """
+    get post url from etree object
+    :param etroot: lxml.etree object
+    :return: url
+    """
+    post_url = "http:" + etroot.find('post_url').text
+    return post_url
+
+
+def get_value_from_xml(etroot):
+    """
+    get keys and values as array object from lxml.etree object
+    :param etroot:lxml.etree object
+    :return name, value: both are array object.
+    """
+    keys = []
+    value = []
+    for child in etroot.iter('parameter'):
+        keys.append(child.find('name').text)
+        value.append(child.find('value').text)
+    return keys, value
