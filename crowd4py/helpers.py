f06e02e890a59ab597649f7cdf697cdc3455c5ab
sshayashi <sshayashi0208@gmail.com>
2017-06-01 17:32:22 +0900

    _FACT1___のプレフィックスの取り付け取り外しのメソッドを作成した

--- crowd4py/helpers.py
+++ crowd4py/helpers.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_post_url(etroot):
     """
     get post url from etree object
@@ -20,3 +23,16 @@ def get_value_from_xml(etroot):
         keys.append(child.find('name').text)
         value.append(child.find('value').text)
     return keys, value
+
+
+def attach_prefix(string):
+    prefix = '_FACT1___'
+    pattern = '^_FACT1___'
+    match = re.search(pattern=pattern, string=string)
+    return prefix+string if match else string
+
+
+def detach_prefix(string):
+    pattern = '^_FACT1___'
+    detach_string = re.sub(pattern=pattern, string=string)
+    return detach_string
