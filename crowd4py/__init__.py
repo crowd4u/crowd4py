77814e21868c59bec6ae417abf7a4bab32fdf3d3
sshayashi <sshayashi0208@gmail.com>
2017-06-04 18:17:33 +0900

    crowd4pyのinitを書き換えた

--- crowd4py/__init__.py
+++ crowd4py/__init__.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+import configparser
+config = configparser.ConfigParser()
+config.read('../config.ini')
+oahu = config['oahu']
+
+USERID = oahu.get('USERID', '')
+PASSWORD = oahu.get('PASSWOR', '')
+API_ROOT = oahu.get('API_ROOT', 'http://crowd4u.org')
+USERINFO = {
+    "requester": oahu.get('requester', '-1'),
+    "from_app": 1,
+    "_machine_language": "ja_JP",
+    "group_id": oahu.get('group_id', '83'),
+    "_user_token": oahu.get('_user_token', '')}
+
 from .api import API
 from .task import Task
 from . import helpers
+
