e7a2b436c332e7fad57483d725cb4a46fff0e6aa
sshayashi <sshayashi0208@gmail.com>
2017-06-04 10:30:33 +0900

    sampleを更新

--- sample/random_answer.py
+++ sample/random_answer.py
@@ -3,20 +3,19 @@ import crowd4py
 import threading
 
 user_info = {
-    'requester': 00,
+    'requester': 818,
     'from_app': 1,
     '_machine_language': 'ja_JP',
-    'group_id': 00,
+    'group_id': 83,
     '_user_token': ''}
 
-project_name = ''
-relation_name = ''
+project_name = 'CompTestV2'
+relation_name = '_Task_Bibrecord_IdentificationTask'
 training_data = []
 
 
 def example_predict(data):
     """
-
     :param data:
     :return: ans_data
     """
@@ -30,17 +29,16 @@ def poring_training_data(api):
 
 if __name__ == '__main__':
     api = crowd4py.API(user_info=user_info, project_name=project_name, relation_name=relation_name)
-    data = api.get_task()
+    task = api.get_task()
 
     # マルチスレッドでポーリング
-    t = threading.Thread(target=poring_training_data(api))
-    t.daemon = True
-    t.start()
+    # t = threading.Thread(target=poring_training_data(api))
+    # t.daemon = True
+    # t.start()
 
-    ans_data = example_predict(data)
+    ans_data = example_predict(task.data)
 
     if ans_data is None:
-        api.request_answer(data['tid'])
+        api.request_answer(task.data['tid'])
     else:
-        api.post_answer(data['tid'], ans_data)
-
+        api.post_answer(task.post_url, ans_data)
