21bf17c31b080ee56d4b5cd5ad0cb001d1f14920
sshayashi <sshayashi0208@gmail.com>
2017-06-04 18:18:20 +0900

    apiの空ファイルを作成した

---
+++ tests/test_api.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+
+
+class Crowd4pyApiTests(unittest.TestCase):
+    """Crowd4py api.py tests"""
+
+    def test_get_task(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
