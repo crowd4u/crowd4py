006caa0e4e2c58d39bdf96289af8af9d6552e93a
kobayang <naokiod@gmail.com>
2017-07-21 19:05:57 +0900

    APIの呼び出し引数をユーザー側で全て設定する

--- crowd4py/__init__.py
+++ crowd4py/__init__.py
@@ -1,21 +1,4 @@
 # -*- coding: utf-8 -*-
-import configparser
-import os
-config = configparser.ConfigParser()
-config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../config.ini'))
-config.read(config_file)
-oahu = config['oahu']
-
-USERID = oahu.get('USERID', '')
-PASSWORD = oahu.get('PASSWORD', '')
-API_ROOT = oahu.get('API_ROOT', 'http://crowd4u.org')
-USERINFO = {
-    "requester": oahu.get('requester', '-1'),
-    "from_app": 1,
-    "_machine_language": "ja_JP",
-    "group_id": oahu.get('group_id', '83'),
-    "_user_token": oahu.get('_user_token', '')}
-
 from .api import API
 from .task import Task
 from . import helpers
--- crowd4py/api.py
+++ crowd4py/api.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from crowd4py import API_ROOT, USERID, PASSWORD
-
 import requests
 from . import helpers
 from .task import Task
@@ -10,15 +8,31 @@ import lxml.etree as et
 class API:
     """crowd4u API"""
 
-    def __init__(self, user_info='', project_name='', relation_name=''):
+    def __init__(self, api_info={}, project_info={}):
         """API instance Constructor
-        :param user_info:
-        :param project_name:
-        :param relation_name:
+        :param api_info:
+        :param project_info:
         """
+        user_id = api_info.get('USERID', '')
+        password = api_info.get('PASSWORD', '')
+        api_root = api_info.get('API_ROOT', 'http://crowd4u.org')
+        user_info = {
+            "requester": api_info.get('REQUESTER', '-1'),
+            "from_app": 1,
+            "_machine_language": "ja_JP",
+            "group_id": api_info.get('GROUP_ID', '83'),
+            "_user_token": api_info.get('USER_TOKEN', '')
+        }
+
+        self.user_id = user_id
+        self.password = password
+        self.api_root = api_root
+        self.user_info = user_info
+
+        project_name = project_info.get('PROJECT_NAME', '')
+        relation_name = project_info.get('RELATION_NAME', '')
 
         self.cookies = ""
-        self.user_info = user_info
         self.project_name = project_name
         self.relation_name = relation_name
 
@@ -31,13 +45,13 @@ class API:
         params = {"project_name": self.project_name,
                   "relation_name": self.relation_name}
 
-        r = requests.get(url=API_ROOT + endpoint, params=params)
+        r = requests.get(url=self.api_root + endpoint, params=params)
         return r.json() if r.status_code == 200 else None
 
     def get_task(self, debug=False) -> Task:
         task_url = self.get_task_url()
         task_url = task_url.replace('https://', 'http://') if debug else task_url
-        r = requests.get(task_url, auth=(USERID, PASSWORD))
+        r = requests.get(task_url, auth=(self.user_id, self.password))
         self.cookies = r.cookies
         task = Task(xml_text=r.content)
         return task
@@ -48,7 +62,7 @@ class API:
                   "relation_name": "_Priority_Current",
                   "tid": tid,
                   "increment_count": increment_count}
-        r = requests.put(url=API_ROOT + endpoint, auth=(USERID, PASSWORD), params=params)
+        r = requests.put(url=self.api_root + endpoint, auth=(self.user_id, self.password), params=params)
         return True if r.status_code == 200 else False
 
     def post_answer(self, post_url: str, ans_data: dict):
@@ -56,12 +70,12 @@ class API:
         keys = ans_data.keys()
         attached_keys = [helpers.attach_prefix(k) for k in keys]
         post_data = dict(zip(attached_keys, values))
-        r = requests.post(post_url, data=post_data, auth=(USERID, PASSWORD), cookies=self.cookies)
+        r = requests.post(post_url, data=post_data, auth=(self.user_id, self.password), cookies=self.cookies)
         return r.status_code
 
     def get_task_url(self) -> str:
         endpoint = '/task_assignment'
-        r = requests.get(API_ROOT + endpoint, data=self.user_info, auth=(USERID, PASSWORD))
+        r = requests.get(self.api_root + endpoint, data=self.user_info, auth=(self.user_id, self.password))
         json = r.json()
         task_url = json["task_url"]
         assert isinstance(task_url, str)
