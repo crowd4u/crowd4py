eba4d768fbd9e0278b59de4496400880e320e291
SShayashi <sshayashi0208@gmail.com>
2017-07-21 17:28:05 +0900

    テスト時に必要のなくなったメソッドを呼び出していたので修正

--- tests/test_task.py
+++ tests/test_task.py
@@ -12,8 +12,7 @@ class Crowd4pyTaskTests(unittest.TestCase):
     def test_get_value_from_xml(self):
         xml = open("tests/task.xml", encoding='utf-8')
         xml_text = xml.read()
-        xml_text = Task.text_cleaning(xml_text.encode())
-        etroot = et.fromstring(xml_text)
+        etroot = et.fromstring(xml_text.encode())
         data_from_xml = Task.get_value_from_xml(etroot=etroot)
         data = dict(tid='999', _FACT1_relation_name='Test')
         self.assertEqual(data_from_xml['tid'], data['tid'])
@@ -22,8 +21,7 @@ class Crowd4pyTaskTests(unittest.TestCase):
     def test_get_post_url(self):
         xml = open("tests/task.xml", encoding='utf-8')
         xml_text = xml.read()
-        xml_text = Task.text_cleaning(xml_text.encode())
-        etroot = et.fromstring(xml_text)
+        etroot = et.fromstring(xml_text.encode())
         task_post_url = Task.get_post_url(etroot=etroot)
         post_url = "http://oahu.slis.tsukuba.ac.jp/event/CompTest?_task_id=5146153"
         self.assertEqual(first=task_post_url, second=post_url)
