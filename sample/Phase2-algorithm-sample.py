7692ef44963b542055f8b14204eaff6baa21979a
kobayang <naokiod@gmail.com>
2017-08-28 02:55:32 +0900

    Documentations

--- sample/Phase2-algorithm-sample.py
+++ sample/Phase2-algorithm-sample.py
@@ -3,4 +3,4 @@ from utils.phases import *
 from workers.algorithm_worker import AlgorithmWorker
 
 if __name__ == "__main__":
-    phase1(AlgorithmWorker(), limit=1)
+    phase2(AlgorithmWorker(), limit=1)
