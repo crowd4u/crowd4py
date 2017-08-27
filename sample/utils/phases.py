858e8afecd0e52923c0035635609aac0867df123
kobayang <naokiod@gmail.com>
2017-08-28 02:55:32 +0900

    Documentations

--- sample/utils/phases.py
+++ sample/utils/phases.py
@@ -3,6 +3,11 @@ from __init__ import API_CONF, PROJECT_CONF
 from utils.api import API
 from utils.conductor import *
 
+"""
+This program is divided into a learning phase and an answering phase.
+The learning phase is done in phase 1 and the answering phase is done in phase 2
+"""
+
 def phase1(workerInstance=None, limit=1):
     api = API(api_info=API_CONF, project_info=PROJECT_CONF)
     TrainingConductor(api, workerInstance, limit).carryOut()
--- sample/workers/algorithm_worker.py
+++ sample/workers/algorithm_worker.py
@@ -2,10 +2,39 @@
 from workers.worker_base import WorkerBase
 
 class AlgorithmWorker(WorkerBase):
+    """This is the human rule based algorithm worker
+    The worker's algorithm is very simple
+    That check the title of books and same title return true, otherwise false.
+    """
+
     def train(self, book1, book2, result):
+        """algorithm worker no uses training models
+        So train show the data of bibliographics and result
+
+        Parameters
+        ----------
+        book1  : object
+        book2  : object
+        result : object
+            book1 and book2 is the bibliographic data that has title and isbn.
+            result is the answer for the task.
+        """
         print(book1, book2, result)
 
     def output(self, book1, book2):
+        """Checking the title of books and same title return true, otherwise false.
+
+        Parameters
+        ----------
+        book1  : object
+        book2  : object
+            book1 and book2 is the bibliographic data that has title and isbn
+
+        Returns
+        -------
+        answer : bool
+        """
+
         print(book1, book2)
         answer = False
         if book1["title"] == book2["title"]:
