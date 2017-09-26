05aa14aa241ddd3cc556c4afac301e4390299054
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2017-09-27 00:47:08 +0900

    updated training sample code for use new fetchTrainingData

--- sample/02_Phase1-ai-sample.py
+++ sample/02_Phase1-ai-sample.py
@@ -3,16 +3,15 @@ from __init__ import API_CONF, PROJECT_CONF
 from utils.api import BibAPI
 from workers.ai_worker import AIWorker
 
-LIMIT = 10
 
 if __name__ == "__main__":
     aiWorker = AIWorker()
     api = BibAPI(api_info=API_CONF, project_info=PROJECT_CONF)
 
     aiWorker.setup()
-    for i in range(LIMIT):
-        book1, book2, result = api.fetchTrainingData(debug=True)
-        aiWorker.train(book1, book2, result)
+    trainingData = api.fetchTrainingData(debug=True)
+    for data in trainingData:
+        aiWorker.train(data['book1'], data['book2'], data['result'])
 
     # Create training model and save model on file
     aiWorker.fit()
