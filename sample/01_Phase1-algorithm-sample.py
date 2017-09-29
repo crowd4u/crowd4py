8fa8436500b51c60393cabfe82c2cdeeb0f7d67b
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2017-09-29 17:15:54 +0900

    use new fetchTrainingData

--- sample/01_Phase1-algorithm-sample.py
+++ sample/01_Phase1-algorithm-sample.py
@@ -3,12 +3,11 @@ from __init__ import API_CONF, PROJECT_CONF
 from utils.api import BibAPI
 from workers.algorithm_worker import AlgorithmWorker
 
-LIMIT = 10
 
 if __name__ == "__main__":
     algorithmWorker = AlgorithmWorker()
     api = BibAPI(api_info=API_CONF, project_info=PROJECT_CONF)
 
-    for i in range(LIMIT):
-        book1, book2, result = api.fetchTrainingData(debug=True)
-        algorithmWorker.train(book1, book2, result)
+    trainingData = api.fetchTrainingData(debug=True)
+    for data in trainingData:
+        algorithmWorker.train(data['book1'], data['book2'], data['result'])
